6efd185d7780a776bb5eaf8773183548a48741da
AlphaBolt <2028014@kiit.ac.in>
2024-02-12 17:31:26 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+
+export default function MyApp() {
+    return (
+      <div>
+        <Header/>
+
+        {/* Add more components here */}
+
+        <Footer/>        
+      </div>
+    );
+  }
