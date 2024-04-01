51be0a473f4c330d33a296026a0f0dca295dbb83
Bratasheel <126228314+Bratasheel@users.noreply.github.com>
2024-04-01 22:40:44 +0530

    Add files via upload

---
+++ job-scrap/source/sign/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOF7Mx-W5-be-jgq-iyyKGa8FuiqMOZaE",
+    authDomain: "job-portal-016.firebaseapp.com",
+    databaseURL: "https://job-portal-016-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "job-portal-016",
+    storageBucket: "job-portal-016.appspot.com",
+    messagingSenderId: "357059889049",
+    appId: "1:357059889049:web:ddc411d7ab20730ea17508",
+    measurementId: "G-WET3HVV3NW"
+};
+export {firebaseConfig};
